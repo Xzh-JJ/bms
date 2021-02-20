08da690c57f65b84d166b322145b5de3ebaf5462
谢政恒 <xiezhengheng@cloudwalk.com>
2021-02-20 17:30:20 +0800

    初始化项目

---
+++ src/views/Home/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home"
+       id="components-layout-demo-basic">
+    <a-layout>
+      <a-layout-sider>Sider</a-layout-sider>
+      <a-layout>
+        <a-layout-content>Content</a-layout-content>
+      </a-layout>
+    </a-layout>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+}
+</script>
+
+<style scoped>
+#components-layout-demo-basic {
+  text-align: center;
+}
+#components-layout-demo-basic .ant-layout-header {
+  background: #7dbcea;
+  color: #fff;
+}
+#components-layout-demo-basic .ant-layout-footer {
+  line-height: 1.5;
+}
+#components-layout-demo-basic .ant-layout-sider {
+  background: #3ba0e9;
+  color: #fff;
+  line-height: 120px;
+}
+#components-layout-demo-basic .ant-layout-content {
+  background: rgba(16, 142, 233, 1);
+  color: #fff;
+  min-height: 120px;
+  line-height: 120px;
+}
+#components-layout-demo-basic > .ant-layout {
+  height: 100vh;
+  margin-bottom: 48px;
+}
+#components-layout-demo-basic > .ant-layout:last-child {
+  margin: 0;
+}
+</style>
---
+++ src/views/Login/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login-container">
+
+  </div>
+</template>
+
+<script>
+import './Login.css';
+export default {
+
+}
+</script>
+
+<style>
+</style>
